12d65de1a1722cbfea55394f6f9f46f78df54fa1
Sergei Efimov <snootfornoyeah@yandex.ru>
2025-10-01 11:04:06 +0300

    Лабораторная работа 1

---
+++ LR 1/sum.py
@@ -0,0 +1,14 @@
+nums = [3, 3]
+target = 6
+
+def sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)): 
+            if nums[i] + nums[j] == target:
+                return i, j
+            else: 
+                continue
+    return 'Подходящей пары не найдено' 
+
+print(nums)
+print(sum(nums, target))
