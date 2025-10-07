1da7aeb30da89acbb0d478bad1ff766f5197da17
Sergei Efimov <snootfornoyeah@yandex.ru>
2025-10-07 20:29:10 +0300

    Небольшие изменения ^_^

--- LR 2/test.py
+++ LR 2/test.py
@@ -1,38 +1,24 @@
 import unittest
 
-def bin_tree(height, root):
-    if height == 0:
-        return None
-    
-    dictionary_tree = {
-        'root': root,
-        'left': bin_tree(height - 1, root * 2 + 1),
-        'right': bin_tree(height - 1, root * 2 - 1)
-    }
+def find_sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)): 
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return 'Подходящей пары не найдено' 
 
-    return dictionary_tree
-
-class TestBinTree(unittest.TestCase):
+class TestSum(unittest.TestCase):
     def test_example_1(self):
-        tree = bin_tree(0, 9)
-        self.assertEqual(tree, None)
+        nums = [2, 7, 11, 15]
+        target = 9
+        self.assertEqual(find_sum(nums, target), [0, 1])
     def test_example_2(self):
-        tree = bin_tree(1, 9)
-        self.assertEqual(tree['root'], 9)
-        self.assertEqual(tree['left'], None)
-        self.assertEqual(tree['right'], None)
+        nums = [3, 2, 4]
+        target = 6
+        self.assertEqual(find_sum(nums, target), [1, 2])
     def test_example_3(self):
-        tree = bin_tree(3, 3)
-        # 1 уровень дерева
-        self.assertEqual(tree['root'], 3) 
-        # 2 уровень дерева
-        self.assertEqual(tree['left']['root'], 7)
-        self.assertEqual(tree['right']['root'], 5)
-        # 3 уровень дерева
-        self.assertEqual(tree['left']['left']['root'], 15)
-        self.assertEqual(tree['left']['right']['root'], 13)
-        self.assertEqual(tree['right']['left']['root'], 11)
-        self.assertEqual(tree['right']['right']['root'], 9)
+        nums = [3, 3]
+        target = 6
+        self.assertEqual(find_sum(nums, target), [0, 1])
         
-				
 unittest.main(verbosity = 2)
