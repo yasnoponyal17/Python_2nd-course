3c8cd21ec08c11ed4970b1192af93c6cf3bcf7a3
Sergei Efimov <snootfornoyeah@yandex.ru>
2025-10-08 09:57:06 +0300

    Лабораторная работа 2

--- LR 2/sum.py
+++ LR 2/sum.py
@@ -1,6 +1,3 @@
-nums = [3, 3]
-target = 6
-
 def find_sum(nums, target):
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)): 
@@ -8,5 +5,3 @@ def find_sum(nums, target):
                 return [i, j]
     return 'Подходящей пары не найдено' 
 
-print('Массив чисел: ', nums)
-print('Результат: ', find_sum(nums, target))
