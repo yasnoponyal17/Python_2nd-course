e5b96877f4d8830f1be75f5e2f904fdb0eb6201f
Sergei Efimov <snootfornoyeah@yandex.ru>
2025-11-03 22:44:43 +0300

    Лабораторная работа 5 O_o

--- LR 5/memo.py
+++ LR 5/memo.py
@@ -29,16 +29,13 @@ def fact_recursive_memo(n):
 def fact_iterative_memo(n):
     if n < 0:
         return 'Число не должно быть меньше нуля'
-    if n == 0 or n == 1:
-        return 1
-    
     result = 1
     for i in range(1, n + 1):
         result *= i
 
     return result
 
-def benchmark(func, n, number=1, repeat=3):
+def benchmark(func, n, number=1, repeat=5):
     times = timeit.repeat(lambda: func(n), number=number, repeat=repeat)
     return min(times)
 
@@ -51,20 +48,16 @@ def main():
     times_iterative_memo = []
 
     for n in n_list:
-        fact_recursive_memo.cache_clear()
-        fact_iterative_memo.cache_clear()
-
-        time_recursive = benchmark(fact_recursive, n, number=1000)
-        time_iterative = benchmark(fact_iterative, n, number=1000)
-        time_recursive_memo = benchmark(fact_recursive_memo, n, number=10000)
-        time_iterative_memo = benchmark(fact_iterative_memo, n, number = 10000)
+        time_recursive = benchmark(fact_recursive, n, repeat=5, number=1000)
+        time_iterative = benchmark(fact_iterative, n, repeat=5, number=1000)
+        time_recursive_memo = benchmark(fact_recursive_memo, n, repeat=5, number=1000)
+        time_iterative_memo = benchmark(fact_iterative_memo, n, repeat=5,number=1000)
 
         times_recursive.append(time_recursive)
         times_iterative.append(time_iterative)
         times_recursive_memo.append(time_recursive_memo)
         times_iterative_memo.append(time_iterative_memo)
 
-    plt.subplot(2, 1, 1)
     plt.plot(n_list, times_recursive, 'ro-', label='Рекурсивная', linewidth=2, markersize=4)
     plt.plot(n_list, times_iterative, 'go-', label='Итеративная', linewidth=2, markersize=4)
     plt.plot(n_list, times_recursive_memo, 'bo-', label='Рекурсивная с мемоизацией', linewidth=2, markersize=4)
