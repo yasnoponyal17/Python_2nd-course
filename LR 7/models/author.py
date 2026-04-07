c18bacc5bb3798bc3f873218cc8d080b723ad7fe
Sergei Efimov <snootfornoyeah@yandex.ru>
2026-04-07 20:52:38 +0300

    mmm

--- LR 7/models/author.py
+++ LR 7/models/author.py
@@ -14,17 +14,29 @@ class Author:
     @name.setter
     def name(self, name: str):
         if not isinstance(name, str):
+<<<<<<< Updated upstream
             raise TypeError('Имя должно быть строкой')
         if name.strip() == '':
             raise ValueError('Имя не может быть пустым значением')
+=======
+            raise TypeError("Имя должно быть строкой")
+        if name.strip() == '':
+            raise ValueError("Имя не может быть пустым")
+>>>>>>> Stashed changes
         self._name = name
 
     @group.setter
     def group(self, group: str):
         if not isinstance(group, str):
+<<<<<<< Updated upstream
             raise TypeError('Имя должно быть строкой')
         if group.strip() == '':
             raise ValueError('Имя не может быть пустым значением')
+=======
+            raise TypeError('Группа должна быть строкой')
+        if group.strip() == '':
+            raise ValueError('Группа не может быть пустой')
+>>>>>>> Stashed changes
         self._group = group
         
     
--- LR 7/tests/test_author.py
+++ LR 7/tests/test_author.py
@@ -3,6 +3,7 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+<<<<<<< Updated upstream
 
 from models.author import Author
 
@@ -39,4 +40,43 @@ class TestAuthor(unittest.TestCase):
         with self.assertRaises(ValueError):
             Author("Зайчик Джуди Хопс", "      ")
             
+=======
+from models.author import Author
+
+class TestAuthor(unittest.TestCase):
+	def test_getter(self):
+		author = Author('Меллстрой', 'Группа 1')
+		self.assertEqual(author.name, 'Меллстрой')
+		self.assertEqual(author.group, 'Группа 1')
+
+	def test_setter(self):
+		author = Author('Меллстрой', 'Группа 1')
+		author.name = 'Кьо этость?'
+		author.group = 'Группа 77'
+		self.assertEqual(author.name, 'Кьо этость?')
+		self.assertEqual(author.group, 'Группа 77')
+
+	def test_type(self):
+		with self.assertRaises(TypeError):
+			Author(67, 'Группа 1')
+
+		with self.assertRaises(TypeError):
+			Author('Code10', 10)
+
+	def test_empty_value(self):
+		with self.assertRaises(ValueError):
+			Author('', 'Группа 1')
+
+		with self.assertRaises(ValueError):
+			Author('  ', 'Группа 1')
+
+		with self.assertRaises(ValueError):
+			Author('Джеффри Эпштейн', '')
+
+		with self.assertRaises(ValueError):
+			Author('Джеффри Эпштейн', '   ')
+
+
+
+>>>>>>> Stashed changes
 unittest.main(verbosity=2)
